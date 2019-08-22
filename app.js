27ad6d4611cd9795ddd5e56d74a1fa3b0ea62df8
Sooze16 <slkpes2002@yahoo.com>
2019-08-22 18:15:54 -0500

    initial thoughts - needs re-work

---
+++ app.js
@@ -0,0 +1,35 @@
+// ======Materialize======
+
+// Parallax
+$(document).ready(function() {
+    $('.parallax').parallax();
+});
+
+// Navbar
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+});
+
+// Boxed
+$(document).ready(function() {
+    $('.materialboxed').materialbox();
+});
+
+// Scrollspy
+$(document).ready(function() {
+    $('.scrollspy').scrollSpy({
+        scrollOffset: 50
+    });
+});
+
+// Modals
+$(document).ready(function() {
+    $('.modal').modal();
+});
+
+// =======Waypoints=======
+
+// Sticky
+var sticky = new Waypoint.Sticky({
+    element: $('.navbar')[0]
+})
